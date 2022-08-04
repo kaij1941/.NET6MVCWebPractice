8dcc45602197c38fca2f514353cdad0b65400947
曹凱嵐Jacky <kaij1941@gmail.com>
2022-08-04 16:00:38 +0800

    initial

---
+++ NET6MVCWebPractice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NET6MVCWebPractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
